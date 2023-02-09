80e15141ab5c8ee42b99d17f55e2ae285ff9b695
Jade Freel <jade@ioxconsulting.co.za>
2023-02-09 09:46:45 +0200

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,7 @@
-using System.Text;
 using API.Data;
 using API.Extensions;
-using API.Interfaces;
 using API.Middleware;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,5 +22,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception e)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred during migration");
+}
 
 app.Run();
